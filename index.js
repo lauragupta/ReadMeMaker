c05f95e20e9c1b842725c1890a2a67217b66ed2a
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-24 20:56:28 -0800

    add license function plus year and full name variables

--- index.js
+++ index.js
@@ -78,6 +78,18 @@ inquirer.prompt([
     {
         name: 'license',
         message: 'Choose a license for your project.',
+        type: 'list',
+        choices: ['MIT', 'ISC', 'unlicense']
+    },
+    {
+        name: 'year',
+        message: 'What year is it (in YYYY form, example 2022)?',
+        type: 'input',
+        default: 2022
+    },
+    {
+        name: 'fullName',
+        message: 'What is your full name?',
         type: 'input'
     },
     {
@@ -114,6 +126,8 @@ inquirer.prompt([
     let collaboratorName = response.collaboratorName;
     let collaboratorGitHub = response.collaboratorGitHub;
     let license = response.license;
+    let year = response.year;
+    let fullName = response.fullName;
     let contributing = response.contributng;
     let tests = response.tests;
     let username = response.username;
@@ -121,7 +135,7 @@ inquirer.prompt([
     
     if(creditPeople === true) {
         creditPeople =`## Credits
-        [${collaboratorName}](https://github.com/${collaboratorGitHub})`
+[${collaboratorName}](https://github.com/${collaboratorGitHub})`
         credits = `* [Credits](#credits)`
     }else {
         creditPeople = ``
@@ -130,11 +144,79 @@ inquirer.prompt([
     if(usage === true) {
         usage =`## Usage 
 
-    ![${usageAlt}](assets/${usageImg})`
+![${usageAlt}](assets/${usageImg})`
         use = `* [Usage](#usage)`
     }else {
         usage = ``
         use = ``
+    }
+
+    if(license === MIT) {
+        license = 
+`MIT License
+
+Copyright (c) ${year} ${fullName}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
+    }else if(license === ISC) {
+        license = 
+`ISC License
+
+Copyright (c) ${year} ${fullName}
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.`
+    }else if(license === unlicense) {
+        license = 
+`This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>`
     }
 
     let body = 
@@ -151,7 +233,6 @@ ${use}
 ${credits}
 * [License](#license)
 
-
 ## Installation
 
 ${installation}
@@ -162,7 +243,6 @@ ${creditPeople}
 
 ## License
 ${license}
-[https://choosealicense.com/](https://choosealicense.com/)
 https://img.shields.io/github/license/lauragupta/ReadMeMaker
 
 ## Contributing
@@ -175,7 +255,7 @@ ${contributing}
 ${tests}
 
 ## Questions 
-For questions, contact me at [GitHub](https://github.com/${username} or email me at ${email}
+For questions, contact me at [GitHub](https://github.com/${username}) or email me at ${email}
 Thanks for reading!`
 
 
